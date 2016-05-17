759e4fde3b6fd2db41696569e0cdd3d9a2beaa2f
pl8787 <pl8787@gmail.com>
2016-05-17 11:05:10 +0800

    Add adam updater

--- src/updater/updater_impl-inl.hpp
+++ src/updater/updater_impl-inl.hpp
@@ -7,7 +7,7 @@
 // #include "./sgdsparse_updater-inl.hpp"
 #include "./adagrad_updater-inl.hpp"
 #include "./adadelta_updater-inl.hpp"
-// #include "./adam_updater-inl.hpp"
+#include "./adam_updater-inl.hpp"
 
 namespace textnet {
 namespace updater {
@@ -18,8 +18,8 @@ Updater<xpu, dim>* CreateUpdater_(UpdaterType type, std::map<std::string, Settin
     case kSGD: return new SGDUpdater<xpu, dim>(setting, prnd);
     case kAdagrad: return new AdagradUpdater<xpu, dim>(setting, prnd);
     case kAdaDelta: return new AdaDeltaUpdater<xpu, dim>(setting, prnd);
-	case kSGDStep: return new SGDStepUpdater<xpu, dim>(setting, prnd);
-    // case kAdam: return new AdamInitializer<xpu, dim>(setting);
+    case kSGDStep: return new SGDStepUpdater<xpu, dim>(setting, prnd);
+    case kAdam: return new AdamUpdater<xpu, dim>(setting, prnd);
     // case kSGDSparse: return new SGDSparseInitializer<xpu, dim>(setting);
     default: utils::Error("unknown updater type id : \"%d\"", type); return NULL;
   }
